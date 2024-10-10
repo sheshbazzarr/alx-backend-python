cef3fc1ef3baeeeb0d91172ed7314c7d1d17612c
sheshbazzarr <demerwdani@gmail.com>
2024-10-10 14:01:07 +0000

    update

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,9 @@
+
+''' Discription : 
+                    this
+    Paramaters:  this
+                this
+'''
+import typing List
+def element_lenght(lst:List[str])->List:
+    return [(i, len(i))for i in lst]
