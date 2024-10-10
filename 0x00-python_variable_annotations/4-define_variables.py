ca5829f6faa7e358453ccb8bdf48f941128da079
sheshbazzarr <demerwdani@gmail.com>
2024-10-10 13:01:07 +0000

    update

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+''' 
+    Description: 
+        and 
+            this 
+            and this:
+
+'''
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
