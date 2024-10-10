c338bfe5c56dbb73468f8da0ba69229c967498a4
sheshbazzarr <demerwdani@gmail.com>
2024-10-10 13:21:55 +0000

    update

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+''' 
+    Description : 
+        this 
+        and 
+        that 
+'''
+from typing import List
+
+def sum_list(input_list: List[float])->float:
+    ''' Return output here'''
+    return sum(input_list)
