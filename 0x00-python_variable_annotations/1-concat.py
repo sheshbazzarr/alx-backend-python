32b0656cf9ad4ccfd6238a0cb9519ce47321c34e
sheshbazzarr <demerwdani@gmail.com>
2024-10-10 12:49:51 +0000

    update

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
+''' Description: takes a string 1 and a string arguments
+        and returns a concatenated string 
+            parameters: str1:str
+                        str2:str
+'''
 
-
-def concat(str1: string, str2: string):
+def concat(str1: str, str2: str):
     ''' Return concatenated string'''
     return str1 + str2
 
